cae2bb1e091a8f97ab2552996ecb0473b6ca5523
brice-klein <bcklein3@gmail.com>
2020-11-03 20:18:14 -0500

    Major work to CSS. Added createElement function to App.vue. This renders a new Study component on +Add Study click.

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,85 @@
 <template>
-  <div v-if="this.studies"  id="app">
+  <div v-if="this.studies" id="app">
     <div id="header">
       <span id="study-name-header">Study Name</span>
       <span id="study-id-header">Study ID</span>
       <span id="study-created-header">Created</span>
       <span id="study-completes-header">Completes</span>
     </div>
-    <div class="studies">
-      <ul>
+    <div class="studies-container">
+      <ul ref="studies">
         <li v-for="study in studies.studyData" :key="study.id">
-          <Study :initialName="study.studyName" :initialId="study.id" :initialCreated="study.creationDate" :initialCompletes="study.numCompletes"></Study>
+          <Study
+            :initialName="study.studyName"
+            :initialId="study.id"
+            :initialCreated="study.creationDate"
+            :initialCompletes="study.numCompletes"
+          ></Study>
         </li>
       </ul>
     </div>
+    <br />
+    <button id="add-study" v-on:click="createElement">+Add Study</button>
   </div>
 </template>
 
 <script>
-import Study from './components/Study.vue'
+import Vue from "vue";
+import Study from "./components/Study.vue";
+
+var StudyClass = Vue.extend(Study);
 
 export default {
   components: {
-    'Study': Study
+    Study: Study,
   },
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      studies: []
-    }
+      studies: null,
+    };
   },
-  beforeCreate: function() {
-    fetch('https://www.cxsurveys.com/devtest/getStudyData.php')
-    .then(res => res.json())
-    .then(data => this.studies = data);
-    },
+  beforeCreate: function () {
+    fetch("https://www.cxsurveys.com/devtest/getStudyData.php")
+      .then((res) => res.json())
+      .then((data) =>
+        this.studies ? (this.studies = this.studies) : (this.studies = data)
+      );
+  },
   methods: {
-  }
-}
+    createElement: function () {
+      var newStudy = new StudyClass({
+        propsData: {
+          initialName: "Untitled Study",
+          initialId: String(Math.floor(Math.random() * 100000)),
+          initialCreated: this.getFormattedDate(new Date()),
+          initialCompletes: "0",
+        },
+      });
+      newStudy.$mount();
+      this.$refs.studies.appendChild(newStudy.$el);
+    },
+    getFormattedDate: function (date) {
+      var year = date.getFullYear();
+
+      var month = (1 + date.getMonth()).toString();
+      month = month.length > 1 ? month : "0" + month;
+
+      var day = date.getDate().toString();
+      day = day.length > 1 ? day : "0" + day;
+
+      return month + "/" + day + "/" + year;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  background-color: #2c6992;
+  background-color: #3d78a3;
   width: 100%;
   height: 100%;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -54,36 +89,59 @@ export default {
 #header {
   width: 100%;
   margin-top: 10%;
+  padding-top: 10px;
   display: flex;
 }
 #study-name-header {
-  width: 24%;
+  width: 30%;
+  margin-left: 25px;
   color: white;
   text-align: left;
-  margin-left: 15%;
+  margin-left: 90px;
+  font-size: small;
 }
 #study-id-header {
   width: 10%;
+  margin-left: 3%;
   color: white;
+  font-size: small;
 }
 #study-created-header {
   width: 10%;
-  padding-right: 10%;
+  margin-left: 3.5%;
   color: white;
   text-align: left;
+  font-size: small;
 }
 #study-completes-header {
   width: auto;
+  margin-left: 0;
   color: white;
+  font-size: small;
 }
-.studies {
-  width: 80%;
+.studies-container {
+  width: 84%;
   display: inline-block;
+  padding-right: 6.5%;
+}
+#add-study {
+  height: 25px;
+  position: relative;
+  right: 42%;
+  margin-top: 20px;
+  background-color: #164462;
+  border: none;
+  border-radius: 7px;
+  color: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
 }
 ul {
   list-style-type: none;
-}
-li {
-  width: 80%
+  justify-content: center;
+  align-content: center;
+  margin-right: 40;
+  margin-left: none;
+  padding-inline-start: 0px;
+  padding-inline-end: 60px;
 }
 </style>
--- src/components/Study.vue
+++ src/components/Study.vue
@@ -1,111 +1,158 @@
 <template>
   <div class="study" :id="this.id" v-if="!hide">
-    <div class="name" v-if="!editMode">{{name}}  <button class="change-study-name" v-on:click="changeStudyName"> CHANGE NAME </button> </div>
-    <td><form v-on:submit="changeStudyName"><input type="text" v-model="name" v-if="editMode" lazy></form></td>
-    <div class="id">{{id}}</div>
-    <div class="created-date"> {{created}} </div>
-    <div class="completes"> {{completes}}<button class="add-complete-button" v-on:click="addComplete"> INCREMENT </button> </div>
-    <button class="delete-study-button" v-on:click="hideStudy"> DELETE </button>
+    <div class="name" v-if="!editMode">
+      {{ name }}
+      <button class="fas fa-pencil-alt" v-on:click="changeStudyName"></button>
+    </div>
+    <td>
+      <form v-on:submit="changeStudyName">
+        <input type="text" v-model="name" v-if="editMode" class="input-field" />
+      </form>
+    </td>
+    <div class="id">{{ id }}</div>
+    <div class="created-date">{{ created }}</div>
+    <div class="completes">
+      {{ completes }}
+    </div>
+    <button class="add-complete-button" v-on:click="addComplete">
+      Add Complete
+    </button>
+    <button class="fas" v-on:click="hideStudy">&#xf2ed;</button>
   </div>
-
 </template>
 <script>
 export default {
-  name: 'Study',
+  name: "Study",
   props: {
     initialName: String,
     initialId: String,
     initialCreated: String,
-    initialCompletes: String
+    initialCompletes: String,
   },
-  // props: ['initialName', 'initialId', 'initialCreated', 'initialCompletes'],
-// for completes? vvv This may remove the need to use bulky Number and String operations.
-// cast value as number??? <input v-model="age" number>
-
-
-  // computed: {
-  //   normalizedCompletes: {
-  //     get: function () {
-  //       return Number(this.initialCompletes)
-  //     },
-  //     set: function () {
-  //       this.completes += 1;
-  //     }
-  //   }
-  // },
-  // computed: {
-  //   normalizedCompletes: function() {
-  //     return Number(this.initialCompletes);
-  //   }
-  // },
-  // beforeCreate () {
-  //   var obj = {}
-  //   obj.extend(this.props);
-  //   this
-  // },
-
   // Data is currently being refreshed everytime parent reloads causing the changes to not be persisted
-  data () {
+  data: function () {
     return {
       name: this.initialName,
       id: this.initialId,
-      created: this.initialCreated,
+      created: this.getFormattedDate(new Date(this.initialCreated)),
       completes: this.initialCompletes,
       editMode: false,
-      hide: false
-      };
+      hide: false,
+    };
   },
+  // beforeDestroy: function () {
+  //   if (!this.data) {
+  //     this.data.id = this.initialId;
+  //     this.data.name = this.initialName;
+  //     this.data.created = this.initialCreated;
+  //     this.data.completes = this.initialCompletes;
+  //     this.editMode = false;
+  //     this.hide = false;
+  //   } else {
+  //     this.data = this.data;
+  //   }
+  // },
   methods: {
-    setData: function() {
-
-    },
+    setData: function () {},
     //this would be better as a computed value
-    addComplete: function() {
+    addComplete: function () {
       this.completes = String(Number(this.completes) + 1);
     },
-    hideStudy: function() {
+    hideStudy: function () {
       this.hide = !this.hide;
     },
-    changeStudyName: function() {
+    changeStudyName: function ($event) {
       this.editMode = !this.editMode;
+      this.data.name = $event.input;
+    },
+    getFormattedDate: function (date) {
+      var year = date.getFullYear();
 
-    }
-  }
-}
+      var month = (1 + date.getMonth()).toString();
+      month = month.length > 1 ? month : "0" + month;
+
+      var day = date.getDate().toString();
+      day = day.length > 1 ? day : "0" + day;
+
+      return month + "/" + day + "/" + year;
+    },
+  },
+};
 </script>
 
 <style>
-  #Study {
-    display: inline-block;
-  }
-  .study {
-    display: flex;
-    background-color: #ffffff;
-    height: 25px;
-    border-radius: 7px;
-    margin-bottom: 7px;
-    padding: 7px;
-  }
-  .name {
-    width: 45%;
-    text-align: left;
-    padding-left: 15px;
-  }
-  .id {
-    display: inline-block;
-    width: 10%
-  }
-  .created-date {
-    display: inline-block;
-    padding-right: 10%;
-  }
-  .completes {
-    display: inline-block;
-  }
-  .delete-study-button {
-    display: inline-block;
-  }
-  .hide {
-    display: none;
-  }
+#Study {
+  display: inline-block;
+}
+.study {
+  display: flex;
+  background-color: #ffffff;
+  height: 25px;
+  width: 100%;
+  border-radius: 8px;
+  margin-bottom: 7px;
+  padding: 7px;
+  padding-top: 13px;
+}
+.fa {
+  background-color: transparent;
+  border: none;
+  color: gray;
+  height: auto;
+  width: auto;
+}
+.fa:hover {
+  cursor: pointer;
+}
+.name {
+  width: 43%;
+  text-align: left;
+  padding-left: 15px;
+  font-weight: bold;
+}
+.input-field {
+  width: 422px;
+  margin-left: 10px;
+}
+.id {
+  display: inline-block;
+  width: 10%;
+  font-size: small;
+}
+.created-date {
+  display: inline-block;
+  padding-right: 7.5%;
+  padding-left: 5.5%;
+  font-size: small;
+}
+.completes {
+  display: inline-block;
+  font-size: small;
+  padding-right: 85px;
+  padding-left: 7px;
+}
+.add-complete-button {
+  height: 20px;
+  width: auto;
+  background-color: #d4d4d4;
+  /* margin-right: 90px; */
+  border: none;
+  border-radius: 5px;
+  padding: 5px;
+  color: #396f92;
+  box-shadow: 0px 4px 3px rgba(0, 0, 0, 0.2);
+}
+.fas {
+  background-color: transparent;
+  border: none;
+  margin-left: 20px;
+  color: gray;
+  font-size: 16px;
+}
+.fas:hover {
+  cursor: pointer;
+}
+.hide {
+  display: none;
+}
 </style>
